a2761fe73d21e809890568e6f4b1e85852331cbf
XRene <1017653702@qq.com>
2017-11-16 10:25:27 +0800

    update

--- index.js
+++ index.js
@@ -1,8 +1,115 @@
-const pattern = /<(h[0-9])>(.*)(<\/?h[0-9]>)/ig
-const svgIcon = '<svg aria-hidden="true" class="octicon octicon-link" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>'
+/**
+ * This markdown-it plugin was modified through the markdown-it-anchor(https://github.com/valeriangalliat/markdown-it-anchor) plugin.
+ * It add some custom feature
+ * 
+ * For Example:
+ * 
+ * 1. In the premalinkHref, it add the <a> element of the href attribute #cube-xxxx-anchor
+ * 2. Removing the blank of h1~h6 element's id attribute
+ * 3. Not using unqiue slug for the h1~h6 id, just using the text of the element as id attribute
+ */
 
-module.exports = function (content) {
-  return content.replace(pattern, (matcher, p1, p2, p3) => {
-    return `<${p1}><a href="#${p2}" class="anchor" id="cube-title-${p2}">${svgIcon}</a>${p2}${p3}`
+const string = require('string')
+
+const slugify = s =>
+  string(s).slugify().toString()
+
+const position = {
+  false: 'push',
+  true: 'unshift'
+}
+
+const blankPattern = /\s+/g
+
+const hasProp = ({}).hasOwnProperty
+
+const permalinkHref = slug => `#cube-${slug.replace(blankPattern, '')}-anchor`
+
+const renderPermalink = (slug, opts, state, idx, title) => {
+  const space = () =>
+    Object.assign(new state.Token('text', '', 0), { content: ' ' })
+
+  const linkTokens = [
+    Object.assign(new state.Token('link_open', 'a', 1), {
+      attrs: [
+        ['class', opts.permalinkClass],
+        ['href', opts.permalinkHref(title)],
+        ['aria-hidden', 'true']
+      ]
+    }),
+    Object.assign(new state.Token('html_block', '', 0), { content: opts.permalinkSymbol }),
+    new state.Token('link_close', 'a', -1)
+  ]
+
+  // `push` or `unshift` according to position option.
+  // Space is at the opposite side.
+  linkTokens[position[!opts.permalinkBefore]](space())
+  state.tokens[idx + 1].children[position[opts.permalinkBefore]](...linkTokens)
+}
+
+const uniqueSlug = (slug, slugs) => {
+  // Mark this slug as used in the environment.
+  slugs[slug] = (hasProp.call(slugs, slug) ? slugs[slug] : 0) + 1
+
+  // First slug, return as is.
+  if (slugs[slug] === 1) {
+    return slug
+  }
+
+  // Duplicate slug, add a `-2`, `-3`, etc. to keep ID unique.
+  return slug + '-' + slugs[slug]
+}
+
+const isLevelSelectedNumber = selection => level => level >= selection
+const isLevelSelectedArray = selection => level => selection.includes(level)
+
+const anchor = (md, opts) => {
+  opts = Object.assign({}, anchor.defaults, opts)
+
+  md.core.ruler.push('anchor', state => {
+    const slugs = {}
+    const tokens = state.tokens
+
+    const isLevelSelected = Array.isArray(opts.level)
+      ? isLevelSelectedArray(opts.level)
+      : isLevelSelectedNumber(opts.level)
+
+    tokens
+      .filter(token => token.type === 'heading_open')
+      .filter(token => isLevelSelected(Number(token.tag.substr(1))))
+      .forEach(token => {
+        // Aggregate the next token children text.
+        const title = tokens[tokens.indexOf(token) + 1].children
+          .filter(token => token.type === 'text' || token.type === 'code_inline')
+          .reduce((acc, t) => acc + t.content, '')
+
+        let slug = token.attrGet('id')
+
+        if (slug == null) {
+          // slug = uniqueSlug(opts.slugify(title), slugs)
+          token.attrPush(['id', title.replace(blankPattern, '')])
+        }
+
+        if (opts.permalink) {
+          opts.renderPermalink(slug, opts, state, tokens.indexOf(token), title)
+        }
+
+        if (opts.callback) {
+          opts.callback(token, { slug, title })
+        }
+      })
   })
 }
+
+anchor.defaults = {
+  level: 1,
+  slugify,
+  permalink: false,
+  renderPermalink,
+  permalinkClass: 'header-anchor',
+  permalinkSymbol: '¶',
+  permalinkBefore: false,
+  permalinkHref
+}
+
+module.exports = anchor
